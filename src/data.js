a5edecc26a0501ae72996a14112466b0123a9bbf
justinpatricia <50977541+justinpatricia@users.noreply.github.com>
2021-10-10 19:35:44 +0700

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,112 @@
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Resep Kuliner Khas Sunda",
+    img: "img/product-1.png",
+    price: 10,
+    company: "PATRICIA JUSTIN",
+    info:
+      "Buku Aneka Resep Masakan merupakan buku yang merangkum berbagai resep masakan. Tidak hanya dari Indonesia, juga terdapat dari berbagai daerah maupun negara. Di dalam buku ini anda akan mendapat petunjuk [raltis untuk membuat berbagai masakan dari berbagai daerah atau negara tergantung dari buku yang anda minati. Untuk itu kami menyediakan buku resep kuliner ini sebanyak 1000 buku untuk membantu para ibu-ibu rumah tangga atau untuk setiap orang yang mau belajar masak.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Resep Kuliner Khas Sulawesi",
+    img: "img/product-2.png",
+    price: 16,
+    company: "PATRICIA JUSTIN",
+    info:
+      "Buku Aneka Resep Masakan merupakan buku yang merangkum berbagai resep masakan. Tidak hanya dari Indonesia, juga terdapat dari berbagai daerah maupun negara. Di dalam buku ini anda akan mendapat petunjuk [raltis untuk membuat berbagai masakan dari berbagai daerah atau negara tergantung dari buku yang anda minati. Untuk itu kami menyediakan buku resep kuliner ini sebanyak 1000 buku untuk membantu para ibu-ibu rumah tangga atau untuk setiap orang yang mau belajar masak.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Resep Kuliner Tradisional Indonesia",
+    img: "img/product-3.png",
+    price: 8,
+    company: "PATRICIA JUSTIN",
+    info:
+      "Buku Aneka Resep Masakan merupakan buku yang merangkum berbagai resep masakan. Tidak hanya dari Indonesia, juga terdapat dari berbagai daerah maupun negara. Di dalam buku ini anda akan mendapat petunjuk [raltis untuk membuat berbagai masakan dari berbagai daerah atau negara tergantung dari buku yang anda minati. Untuk itu kami menyediakan buku resep kuliner ini sebanyak 1000 buku untuk membantu para ibu-ibu rumah tangga atau untuk setiap orang yang mau belajar masak.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Resep Kuliner Khas Jawa Barat",
+    img: "img/product-4.png",
+    price: 18,
+    company: "PATRICIA JUSTIN",
+    info:
+      "Buku Aneka Resep Masakan merupakan buku yang merangkum berbagai resep masakan. Tidak hanya dari Indonesia, juga terdapat dari berbagai daerah maupun negara. Di dalam buku ini anda akan mendapat petunjuk [raltis untuk membuat berbagai masakan dari berbagai daerah atau negara tergantung dari buku yang anda minati. Untuk itu kami menyediakan buku resep kuliner ini sebanyak 1000 buku untuk membantu para ibu-ibu rumah tangga atau untuk setiap orang yang mau belajar masak.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Resep kuliner Khas Makassar",
+    img: "img/product-5.png",
+    price: 24,
+    company: "PATRICIA JUSTIN",
+    info:
+      "Buku Aneka Resep Masakan merupakan buku yang merangkum berbagai resep masakan. Tidak hanya dari Indonesia, juga terdapat dari berbagai daerah maupun negara. Di dalam buku ini anda akan mendapat petunjuk [raltis untuk membuat berbagai masakan dari berbagai daerah atau negara tergantung dari buku yang anda minati. Untuk itu kami menyediakan buku resep kuliner ini sebanyak 1000 buku untuk membantu para ibu-ibu rumah tangga atau untuk setiap orang yang mau belajar masak.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Resep Kuliner Chinese",
+    img: "img/product-6.png",
+    price: 17,
+    company: "PATRICIA JUSTIN",
+    info:
+      "Buku Aneka Resep Masakan merupakan buku yang merangkum berbagai resep masakan. Tidak hanya dari Indonesia, juga terdapat dari berbagai daerah maupun negara. Di dalam buku ini anda akan mendapat petunjuk [raltis untuk membuat berbagai masakan dari berbagai daerah atau negara tergantung dari buku yang anda minati. Untuk itu kami menyediakan buku resep kuliner ini sebanyak 1000 buku untuk membantu para ibu-ibu rumah tangga atau untuk setiap orang yang mau belajar masak.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Resep Kuliner Jepang",
+    img: "img/product-7.png",
+    price: 30,
+    company: "PATRICIA JUSTIN",
+    info:
+      "Buku Aneka Resep Masakan merupakan buku yang merangkum berbagai resep masakan. Tidak hanya dari Indonesia, juga terdapat dari berbagai daerah maupun negara. Di dalam buku ini anda akan mendapat petunjuk [raltis untuk membuat berbagai masakan dari berbagai daerah atau negara tergantung dari buku yang anda minati. Untuk itu kami menyediakan buku resep kuliner ini sebanyak 1000 buku untuk membantu para ibu-ibu rumah tangga atau untuk setiap orang yang mau belajar masak.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Resep Kuliner Vegetarian Korea",
+    img: "img/product-8.png",
+    price: 2,
+    company: "PATRICIA JUSTIN",
+    info:
+      "Buku Aneka Resep Masakan merupakan buku yang merangkum berbagai resep masakan. Tidak hanya dari Indonesia, juga terdapat dari berbagai daerah maupun negara. Di dalam buku ini anda akan mendapat petunjuk [raltis untuk membuat berbagai masakan dari berbagai daerah atau negara tergantung dari buku yang anda minati. Untuk itu kami menyediakan buku resep kuliner ini sebanyak 1000 buku untuk membantu para ibu-ibu rumah tangga atau untuk setiap orang yang mau belajar masak.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 10,
+  company: "google",
+  info:
+    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
